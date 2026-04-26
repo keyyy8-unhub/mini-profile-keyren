c371f20dfce8aa9284fa413df73c23ce4d1c2a5e
keyyy8-unhub <keyrenavanti@gmail.com>
2026-04-26 13:31:50 +0000

    first commit profile keyren

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btnSapa');
+const pesan = document.getElementById('pesanSapaan');
+
+btn.addEventListener('click', function() {
+    pesan.innerText = "/☆Hajimemashite! Keyren-desu, yoroshiku onegaishimasu☆♪/";
+});
